f698fe7bb8d0ea188ab2c67acf385bd86f1d0cf6
Jeet-bhardwaj <bhardwajjeet409@gmail.com>
2024-06-19 18:47:00 +0530

    new update

--- video_86/main.js
+++ video_86/main.js
@@ -1,14 +1,12 @@
-// server.mjs
-import { createServer } from 'node:http';
+const http = require('http');
 
-const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World!\n');
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h1>Hello world</h1>');
 });
 
-// starts a simple http server locally on port 3000
-server.listen(3000, '127.0.0.1', () => {
-  console.log('Listening on 127.0.0.1:3000');
-});
+const PORT = process.env.PORT || 3000;
 
-// run with `node server.mjs`
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
