48ed8586c330bfc62055814c820e791bc70a2aef
Jeet-bhardwaj <bhardwajjeet409@gmail.com>
2024-06-25 22:34:10 +0530

    jeet

--- video_88/main.js
+++ video_88/main.js
@@ -2,10 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/home', (req, res) => {
+  res.send('HOME!')
+})
+app.get('/About', (req, res) => {
+  res.send('About')
+})
+
+app.get('/home/:slug', (req, res) => {
+  res.send(`hello ${req.params.slug}`)
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listeing on port ${port}`)
 })
